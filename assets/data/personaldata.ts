271f3bb4fa5a21707de497bdfa1c538d2be9b1e5
mbutlerdesign <52702544+mbutlerdesign@users.noreply.github.com>
2019-08-07 15:38:28 -0400

    Add files via upload

---
+++ assets/data/personaldata.ts
@@ -0,0 +1,9 @@
+import {Butlerm} from '../../app/butlerm';
+
+export const BUTLERM: Butlerm = {
+    studentnum:'991234259',
+    name: 'Mickail Butler',
+    loginname:'butlerm',
+    campus:'Davis Campus',
+    assignment:'Assignment # 4'
+};
